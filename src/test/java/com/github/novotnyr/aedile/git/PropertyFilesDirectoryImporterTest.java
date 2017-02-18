b6231ca0a063a7cba4c2d147f0e8a191570f6678
Robert Novotny <robert.novotny@dcoresystems.com>
2017-02-18 16:49:42 +0100

    Use proper name for unit tests

--- src/test/java/com/github/novotnyr/aedile/git/PropertyFilesDirectoryImporterTest.java
+++ src/test/java/com/github/novotnyr/aedile/git/PropertyFilesDirectoryImporterTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-public class PropertyDirectoryConsulKeyAndValueImporterTest {
+public class PropertyFilesDirectoryImporterTest {
     private ConsulConfigurationRepository repository;
 
     private ConsulClient consulClient;
